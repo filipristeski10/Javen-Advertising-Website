d3e33a1216cb745829a1e0d9002a6030eaa83c7b
Filip Risteski <filipristeski30@gmail.com>
2024-03-29 00:05:22 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <FirstHeroSection />
     </div>
   );
 }
 
 export default App;
+
+function Header() {
+  return (
+    <div className="topHeaderContainer">
+      <div className="topHeader">
+        <Logo />
+        <NavBar />
+        <ContactDetails />
+      </div>
+    </div>
+  );
+}
+
+function Logo() {
+  return <div className="logo">SOCIAL ME</div>;
+}
+
+function NavBar() {
+  return (
+    <ul className="topHeaderList">
+      <li>HOME</li>
+      <li>ABOUT</li>
+      <li>SERVICES</li>
+      <li>COURSE</li>
+      <li>BLOG</li>
+    </ul>
+  );
+}
+
+function ContactDetails() {
+  return (
+    <div className="topHeaderContactDetails">
+      <div className="topHeaderContactDetailsSocials">
+        <img src="/icons8-instagram (14).svg" alt="instagram" />
+        <img src="/icons8-facebook (12).svg" alt="facebook" />
+      </div>
+      <button className="topHeaderContactButton">
+        <a href="https://www.example.com">CONTACT</a>
+      </button>
+    </div>
+  );
+}
+
+/* This above is the Header of the website*/
+
+function FirstHeroSection() {
+  return (
+    <div className="heroSection">
+      <div className="herSectionContainer">
+        <LeftColumnHeroSection />
+        <RightColumnHeroSection />
+      </div>
+    </div>
+  );
+}
+
+function LeftColumnHeroSection() {
+  return (
+    <div>
+      <LeftColumnHeroSectionText />
+      <LeftColumnHeroSectionButton />
+    </div>
+  );
+}
+
+function RightColumnHeroSection() {
+  return <img src="/reading.jpg" alt="women reading" />;
+}
+
+function LeftColumnHeroSectionText() {
+  return (
+    <div>
+      <h2>I'am here to shine up your social media</h2>
+      <h4>awydgauywgdauywgduaywgduawydgauywgduaywgduaywdguawydgauwydawd</h4>
+    </div>
+  );
+}
+
+function LeftColumnHeroSectionButton() {
+  return (
+    <button>
+      <a href="#">WHEN DO WE BEGIN?</a>
+    </button>
+  );
+}
